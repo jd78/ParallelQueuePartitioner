bf12b3c8bd0d33c12cb9f3d532284ffa0f119438
Gennaro Del Sorbo <gennaro.delsorbo@gmail.com>
2015-06-26 11:24:57 +0000

    logger path

--- Application/Logger.js
+++ Application/Logger.js
@@ -1,10 +1,24 @@
 var winston = require("winston");
+var fs = require("fs");
+var util = require("util");
+
+var binPath = "bin";
+var servicePath = "parallel-queue-partitioner";
+var fullpath = util.format("./%s/%s", binPath, servicePath);
+
+if (!fs.existsSync(util.format("./%s", binPath))) {
+	fs.mkdirSync(util.format("./%s", binPath));
+}
+
+if (!fs.existsSync(fullpath)) {
+	fs.mkdirSync(fullpath);
+}
 
 var logger = new (winston.Logger)({
     transports: [
       new (winston.transports.Console)(),
       new (winston.transports.File)({ 
-          filename: 'partitioner.log',
+          filename: util.format("%s/partitioner.log", fullpath),
           handleExceptions: true,
           exitOnError: false,
           level: 'error' //info, warning, error
