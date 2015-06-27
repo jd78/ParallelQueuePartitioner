ae9f1892bd63e24c1c3fd36fdcd1b81287b11513
Gennaro Del Sorbo <gennaro.delsorbo@gmail.com>
2015-06-27 18:57:51 +0000

    code improved, verified and fixed

--- Application/Logger.js
+++ Application/Logger.js
@@ -18,10 +18,12 @@ var logger = new (winston.Logger)({
     transports: [
       new (winston.transports.Console)(),
       new (winston.transports.File)({ 
-          filename: util.format("%s/partitioner.log", fullpath),
+          filename: util.format("%s/pid-%s-partitioner.log", fullpath, process.pid),
           handleExceptions: true,
           exitOnError: false,
-          level: 'error' //info, warning, error
+          level: 'error', //info, warning, error
+          maxsize: 625000,
+          zippedArchive: true
       })
     ]
 });
--- Samples/readme-sum-demo.js
+++ Samples/readme-sum-demo.js
@@ -2,12 +2,14 @@ var parallelQueuePartitioner = require("parallel-queue-partitioner");
 var Partitioner = parallelQueuePartitioner.Partitioner;
 var cluster = require("cluster");
 var q = require("q");
+var logger = require("./Application/Logger");
+logger.transports.file.level = 'debug';
 
 if(cluster.isWorker) {
     parallelQueuePartitioner.registerJob('sum', function(job){
         return q.Promise(function(resolve){
             var sum = job.data.one + job.data.two;
-            console.log("partition: %d, pid: %d, sum: %d", job.partitionId, process.pid, sum);
+            logger.debug("partition: %d, pid: %d, sum: %d", job.partitionId, process.pid, sum);
             resolve();
         });
     });
@@ -16,7 +18,7 @@ if(cluster.isWorker) {
         return q.Promise(function(resolve){
             setTimeout(function(){
                 var sum = job.data.one + job.data.two;
-                console.log("partition: %d, pid: %d, sum: %d", job.partitionId, process.pid, sum);
+                logger.debug("partition: %d, pid: %d, sum: %d", job.partitionId, process.pid, sum);
                 resolve();
             }, 1000);
     	});
@@ -28,30 +30,21 @@ if(cluster.isMaster)
     
 function Start(){
     var partitioner = new Partitioner({
-        numberOfWorkers: 8
+        numberOfWorkers: 4,
+        loggerLevel: 'debug'
     });
     
     setTimeout(function(){
-		for(var i=1; i<50; i++) {
+		for(var i=1; i<5000; i++) {
             partitioner.enqueueJob({
                 id: i,
                 partitionId: i%8, //Spreading across the workers
                 type: "sum",
                 data: { one: i, two: i+1 }
             }, function(){
-				console.log("sum job ended");
+				logger.debug("sum job %d ended", i);
 			});
         }
 		
-		for(var i=1; i<50; i++) {
-            partitioner.enqueueJob({
-                id: i,
-                partitionId: 10, //Only one process will execute these 50 messages in sequence
-                type: "delayedSum",
-                data: { one: i, two: i*i }
-            }, function(){
-				console.log("sequential job ended");
-			});
-        }
     }, 2000); //Atbitraty deleyer to wait all forks are completed
 }
