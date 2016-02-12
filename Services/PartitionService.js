74afbd14d2b16973ef891ad344bf9003e8449860
Gennaro Del Sorbo <gennaro.delsorbo@gmail.com>
2016-02-12 17:14:31 +0000

    partitionService refactor

--- Services/PartitionService.js
+++ Services/PartitionService.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const _ = require("underscore");
 const moment = require("moment");
 const q = require("q");
@@ -7,8 +9,8 @@ const util = require("util");
 const Lock = require("../Application/ExecuteLocked");
 const lock = new Lock();
 
-var partitions = [];
-var cleanIdlePartitionsAfterMinutes;
+let _partitions = new Map();
+let _cleanIdlePartitionsAfterMinutes;
 
 function Partition(partitionId, worker) {
     var self = this;
@@ -19,38 +21,35 @@ function Partition(partitionId, worker) {
     setInterval(function() {
         var logger = Logger.instance();
         logger.info(util.format("Partition cleanup fired for %d", partitionId));
-        if(self.updatedAt <= moment().utc().subtract(cleanIdlePartitionsAfterMinutes, 'minutes').format())
+        if(self.updatedAt <= moment().utc().subtract(_cleanIdlePartitionsAfterMinutes, 'minutes').format())
             lock.execWrite(function(){ 
                 return q.Promise(function(resolve){
-                  partitions.splice(_.findIndex(partitions, self), 1);
+                  _partitions.delete(self.partitionId)
                   logger.info(util.format("Partition %d cleaned", partitionId));
                   resolve();
                 });
             });
         logger.info(util.format("Partition cleanup completed for %d", partitionId));
-    }, cleanIdlePartitionsAfterMinutes * 60 * 1000);
+    }, _cleanIdlePartitionsAfterMinutes * 60 * 1000);
 }
 
 function PartitionService(cleanIdlePartitions){
     if(cleanIdlePartitions === null || cleanIdlePartitions === undefined || isNaN(parseInt(cleanIdlePartitions)) || cleanIdlePartitions <= 0)
         throw new Error("cleanIdlePartitionsAfterMinutes required integer greater than 0");
     
-    cleanIdlePartitionsAfterMinutes = cleanIdlePartitions;
+    _cleanIdlePartitionsAfterMinutes = cleanIdlePartitions;
 }
 
 PartitionService.prototype.get = function(partitionId) {
     return lock.execRead(function(){
        return q.Promise(function(resolve, reject){
             try {
-                var maybePartition = _.findWhere(partitions, {partitionId: partitionId});
+                if(!_partitions.has(partitionId))
+                    return resolve(null)
                 
-                if(maybePartition == undefined)
-                    return resolve(null);
-                
-                maybePartition.updatedAt = moment().utc().format();
-                
-                resolve(maybePartition);
-                
+                let partition = _partitions.get(partitionId)
+                partition.updatedAt = moment().utc().format();
+                resolve(partition);
             } catch(ex){
                 Logger.instance().error(ex);
                 reject(ex);
@@ -63,7 +62,7 @@ PartitionService.prototype.push = function(partitionId, worker) {
     return lock.execWrite(function() {
         return q.Promise(function(resolve){
             var partition = new Partition(partitionId, worker);
-            partitions.push(partition);
+            _partitions.set(partitionId, partition);
             resolve(partition);
         });
     });
