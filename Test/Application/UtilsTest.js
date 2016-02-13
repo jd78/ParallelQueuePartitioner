834b7b3a5e25a020af5e001bfda6e76b864b1546
Gennaro Del Sorbo <JD78@Gennaros-MacBook-Pro.local>
2016-02-13 01:15:08 +0000

    UtilTest added

---
+++ Test/Application/UtilsTest.js
@@ -0,0 +1,30 @@
+"use strict"
+
+require("should");
+const utils = require("../../Application/Utils")
+
+describe("Utils Test", () => {
+    it("If first is null, get the second", () => {
+        let first = null;
+        let second = 1;
+        utils.coalesce(first, second).should.equal(second);
+    })
+    
+    it("If first is undefined, get the second", () => {
+        let first;
+        let second = 1;
+        utils.coalesce(first, second).should.equal(second);
+    })
+    
+    it("If first is false, get first", () => {
+        let first = false;
+        let second = 1;
+        utils.coalesce(first, second).should.equal(first);
+    })
+    
+    it("If first is not null, get first", () => {
+        let first = 1;
+        let second = 2;
+        utils.coalesce(first, second).should.equal(first);
+    })
+});
