8a77b48c14bf056a2e5b41f91deff1a85177d386
Gennaro Del Sorbo <JD78@Gennaros-MacBook-Pro.local>
2016-02-13 18:15:37 +0000

    refactor

--- Samples/test-no-broker.js
+++ Samples/test-no-broker.js
@@ -1,39 +1,39 @@
 "use strict"
 
-const Partitioner = require("../Partitioner").Partitioner;
-const cluster = require("cluster");
-const registerJob = require("../Partitioner").registerJob;
+const Partitioner = require("../Partitioner").Partitioner
+const cluster = require("cluster")
+const registerJob = require("../Partitioner").registerJob
 
 if(cluster.isWorker) {
     registerJob('test', job => {
         return new Promise(resolve => {
-            console.log("the job has been executed by " + process.pid);
-            resolve();
-        });
-    });
+            console.log("the job has been executed by " + process.pid)
+            resolve()
+        })
+    })
     registerJob('sum', job => {
         return new Promise(resolve => {
-            var sum = job.data.one + job.data.two;
-            console.log("partition: %d, pid: %d, sum: %d", job.partitionId, process.pid, sum);
-            resolve();
-        });
-    });
+            var sum = job.data.one + job.data.two
+            console.log("partition: %d, pid: %d, sum: %d", job.partitionId, process.pid, sum)
+            resolve()
+        })
+    })
     registerJob('slow', job => {
         return new Promise(resolve => {
-            console.log("slow job started. Id: %d, Partition: %d, pid: %d", job.id, job.partitionId, process.pid);
+            console.log("slow job started. Id: %d, Partition: %d, pid: %d", job.id, job.partitionId, process.pid)
             for(var i=0; i<999999999; i++){
             
             }
-            console.log("slow job completed. Id: %d, Partition: %d, pid: %d", job.id, job.partitionId, process.pid);
-            resolve();
-        });
-    });
+            console.log("slow job completed. Id: %d, Partition: %d, pid: %d", job.id, job.partitionId, process.pid)
+            resolve()
+        })
+    })
 }
 
 let start = () => {
     var partitioner = new Partitioner({
         numberOfWorkers: 4
-    });
+    })
     
     setTimeout(function(){
         for(var i=1; i<20; i++) {
@@ -46,16 +46,16 @@ let start = () => {
                     data: { }
                 }, function(err){
                     if(err !== undefined)
-                        console.log(err);
+                        console.log(err)
                         
-                });
+                })
             }else{
                 partitioner.enqueueJob({
                     id: i,
                     partitionId: i%3,
                     type: "slow",
                     data: { }
-                });
+                })
             }
         }
         
@@ -69,12 +69,12 @@ let start = () => {
                     two: i+1
                 }
             }, function(){
-                console.log("CALLBACK CALLED");
-            });
+                console.log("CALLBACK CALLED")
+            })
         }
         
-    }, 2000);
+    }, 2000)
 }
 
 if(cluster.isMaster)
-    start();
+    start()
